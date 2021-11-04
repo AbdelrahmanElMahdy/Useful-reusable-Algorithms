827bbbe2784894195989e2798a1f724a99c931ea
Abdelrahman Mahdy <56981627+AbdelrahmanElMahdy@users.noreply.github.com>
2021-11-04 12:30:50 +0200

    Update reuse.js

--- reuse.js
+++ reuse.js
@@ -10,8 +10,9 @@
 //   { price: 33.333333333333336, date: '2021-10-28' },
 //   { price: 33.333333333333336, date: '2021-11-14' },
 //   { price: 33.333333333333336, date: '2021-11-28' },
-
-function getDatesPrice(noPlans, totalPrice, formalDate=false){
+// days >> which days if the month must be 2 days 
+// allowed period >> period that if the current day is before it we shift the installment to next sechedual day 
+function getDatesPrice(noPlans, totalPrice, formalDate=false,days=[14,18],allowedPeriod=4){
   price= totalPrice / noPlans ;     
   let currentDate= new Date(Date.now());
 
@@ -19,20 +20,28 @@ function getDatesPrice(noPlans, totalPrice, formalDate=false){
 
   let year= currentDate.getFullYear();
   let month = currentDate.getMonth();
-  let currentDay= currentDate.getDay();
+  let currentDay= currentDate.getDay() 
   
-  let day14or28 = currentDay > 9 && currentDay < 23 ? 28 : 14;
+  let dayFirstOrSecond = currentDay > days[0]-allowedPeriod && currentDay < days[1]-allowedPeriod ? days[1] : days[0];
   for (let noDownPay=0; noDownPay<noPlans; noDownPay++){
-    let date =`${year}-${month}-${day14or28}`;
+    let date =`${year}-${month}-${dayFirstOrSecond}`;
 	
 	if (formalDate) {installmentsObj.push({price,date :new Date(date)})}
 	else {installmentsObj.push({price,date})};
     
-    if( day14or28 ===28 ) month++;
+    
+
+    if( dayFirstOrSecond ===days[1] ) month++;
     if(month ===13){
       month=1;
       year++;
     }
+
+    dayFirstOrSecond =  dayFirstOrSecond === days[0] ? days[1] : days[0];
+  } 
+  return installmentsObj;
+}
+
 
     day14or28 =  day14or28 === 14 ? 28 : 14;
   } 
